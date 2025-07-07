fa274ca1c1a3a7eba35009bb09feb3a02db9a341
itsglobally1324 <itsglobally1324@gmail.com>
2025-07-07 01:39:41 +0000

    e

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import localFont from "next/font/local";
-/*
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -12,11 +11,10 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-*/
 const minecraft = localFont(
   {
     src: "./Minecraft.woff2",
-    variable: "--font-minecraft",
+    variable: `--${geistSans.variable} ${geistMono.variable}`,
   }
 )
 
